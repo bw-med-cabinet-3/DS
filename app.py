0daaaa2282efde690a10cb85a8f638f2c24048bd
ethanmjansen <ethanm.jansen@gmail.com>
2020-02-04 10:32:16 -0700

    Create if name=main

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ import sqlite3
 import pandas as pd
 
 
-app = Flask(__name__)
-    
+def create_app():
+  app = Flask(__name__)
+  @app.route('/')
+  def hello_world():
+    return 'Hello world'
 
-@app.route('/')
-def hello_world():
-  return 'Hello world'
+  return app
+
+
+if __name__ == "__main__":
+  app = create_app()
