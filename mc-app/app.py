c94a605c02815ace4e834184fa51af557f7c9aa8
ethanmjansen <ethanm.jansen@gmail.com>
2020-02-04 10:54:46 -0700

    test results query

--- mc-app/app.py
+++ mc-app/app.py
@@ -3,11 +3,24 @@ import sqlite3
 import pandas as pd
 
 
+df = pd.read_csv('cannabis.csv')
+conn = sqlite3.connect('cannabis.sqlite3')
+curs = conn.cursor()
+drop_table = '''DROP TABLE cannabis;'''
+curs.execute(drop_table)
+df.to_sql('cannabis', conn)
+
+query1 = '''SELECT * from cannabis;'''
+
+
+result_query = curs.execute(query1).fetchall()
+
+
 def create_app():
   app = Flask(__name__)
   @app.route('/')
   def hello_world():
-    return 'Hello world'
+    return str(result_query)
 
   return app
 
