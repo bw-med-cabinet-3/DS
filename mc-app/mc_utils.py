2b2702bbdb5a85175b323e5eba8bce2157e35291
ethanmjansen <57106932+ethanmjansen@users.noreply.github.com>
2020-02-05 17:21:57 -0700

    Update mc_utils.py

--- mc-app/mc_utils.py
+++ mc-app/mc_utils.py
@@ -1,83 +1,75 @@
-'''
-Med Cabinet app functionality 
-'''
+## import libraries
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def combine_features(row):
-    '''Combine features into block of text.
-    '''
-    return row['Strain'] + " " + row['Type'] + " " + row['Rating'] + " " + row['Effects'] + " " + row['Flavor'] + " " + row['Description']
+## get the data
+df = pd.read_csv('cannabis.csv')
+df.head()
 
+## functions
 
-def fill_nans(df):
-    '''Features are the column names for the features you want to include
-       This function fills nans with an empt string.
-    '''
-    features = ['Strain', 'Type', 'Rating', 'Effects', 'Flavor', 'Description']
-    for feature in features:
-        df[feature] = df[feature].fillna('')
-    return df
-
-
-def get_index_from_strain(strain):
-    '''Get an index from your strains.
-    '''
-    strain_index = df[df['Strain'] == strain_user_likes].index[0]
-    return strain_index
-
-
-def get_strain_from_index(index):
-    '''Get strains from your index.
-    '''
-    strain_name = df.iloc[index][0]
-    return strain_name
-
-
-def column_contains(df, column, query):
-    '''Returns rows based on condition.
-    '''
-    return df[df[column].str.contains(query)]
-
-
-def drop_duplicates(df):
-    '''Drops duplicate rows in dataframe
-    '''
+## remove duplicates
+def remove_duplicates(df):
     df = df.drop_duplicates(keep='first')
     return df
 
+## convert all strain names to all lowercase 
+def lower_case_column(column_name):
+    return df[column_name].str.lower()
 
-# create a count matrix from new combined column
+## search by strain keyword
+def search_strains(keyword):
+    return df[df['Strain'].str.contains(keyword)]
 
-# instantiate an object of CountVectorizer class
-cv = CountVectorizer()
+## get strain name from index
+def get_strain_from_index(index):
+    strain_name = df.iloc[index][0]
+    return strain_name
 
-# apply fit transform onto our combined column
-count_matrix = cv.fit_transform(df['combined_features'])
+## get index number from strain
+def get_index_from_strain(strain):
+    strain_index = df[df['Strain'] == strain].index[0]
+    return strain_index 
 
-# calculate cosine-similarity between items
-cosine_sim = cosine_similarity(count_matrix)
+## combine features into one column
+def combine_features(row):
+    return row['Strain'] +" "+ row['Type'] + " "+ row['Rating'] + " " + row['Effects'] + " " + row['Flavor'] + " " + row['Description']
 
-strain_user_likes = 'Skywalker'
+## these are the columns we are going to use
+features = ['Strain', 'Type', 'Rating', 'Effects', 'Flavor', 'Description']
 
-strain_index = get_index_from_strain(strain_user_likes)
+## drop duplicates
+df = remove_duplicates(df)
 
-# find similar strains
-# enumerate assigns an index
-# list casts our result as list
-similar_strains = list(enumerate(cosine_sim[strain_index]))
+## convert all columns to string for nlp
+df = df.astype(str)
 
-# sort similar strains so most close are at the top
-sorted_similar_strains = sorted(
-    similar_strains, key=lambda x: x[1], reverse=True)
+## make a combined features column
+df['combined_features'] = df.apply(combine_features, axis=1)
 
-# print 10 closest strain names
-i = 0
-result = for strain in sorted_similar_strains:
-    print(get_strain_from_index(strain[0]))
-    i = i + 1
-    if i > 11:
-        break
+## fill nans with emtpy string
+for feature in features:
+    df[feature] = df[feature].fillna('')
+
+## make strain titles lowercase for easier search
+df['Strain'] = lower_case_column('Strain')
+
+## the function
+def recommend_strains(strain, num_of_strains=5):
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(df['combined_features'])
+    cosine_sim = cosine_similarity(count_matrix)
+    strain_index = get_index_from_strain(strain)
+    similar_strains = list(enumerate(cosine_sim[strain_index]))
+    sorted_similar_strains = sorted(similar_strains, key= lambda x:x[1], reverse=True)
+    recommended_strains = []
+    i = 0
+    for strain in sorted_similar_strains:
+        recommended_strains.append(get_strain_from_index(strain[0]))
+        i = i + 1
+        if i > num_of_strains:
+            break
+    return recommended_strains
