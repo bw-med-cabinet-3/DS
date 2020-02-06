09f1159669e40e8108cfafec685d6c8b5f861be1
ethanmjansen <ethanm.jansen@gmail.com>
2020-02-06 11:10:09 -0700

    Heroku deploy test

--- mc_app/app.py
+++ mc_app/app.py
@@ -2,12 +2,13 @@ from flask import Flask, request, jsonify
 import pandas as pd
 from mc_app import mc_utils
 import pickle
+
 pickle_dict = pickle.load(open("marijuana_dict.p", "rb"))
 
 
 def create_app():
     app = Flask(__name__)
-    @app.route('/strain', methods=['GET'])
+    @app.route('/', methods=['GET'])
     def myfunc():
       #strain = strain or request.values['strain']
       return jsonify({'strain':pickle_dict['100-og']})
